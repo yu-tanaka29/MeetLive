e022d4b9e31cc424eefe3653468cbff6863ecfe8
yuki.tanaka <yu-tanaka@i-enter.co.jp>
2022-06-02 14:48:01 +0900

    Initial Commit

---
+++ MeetLive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeetLive
+//
+//  Created by 田中 勇輝 on 2022/06/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
